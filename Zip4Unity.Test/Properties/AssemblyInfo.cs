c942c9ca5cdbb60281f6a82ec82b4721f42003e5
Taegyeong Kim <hide1202@naver.com>
2017-06-27 23:49:14 +0900

    Add unit test

---
+++ Zip4Unity.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Zip4Unity.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zip4Unity.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79541277-16bf-46a7-866b-ff4fef2ce3c9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Zip4Unity.Test/UnzipTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Ionic.Zip;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zip4Unity.Test
+{
+    [TestClass]
+    public class UnzipTest
+    {
+        private const string TestResourcesDirPath = @".\TestResources";
+        private const string MoonZipFileName = @"moon.zip";
+        private const string MoonJpgFileName = @"moon.jpg";
+        private const string MoonOriginalJpgFileName = @"moon_original.jpg";
+
+        [TestMethod]
+        public void MoonUnzipTest()
+        {
+            var moonZipFilePath = Path.Combine(TestResourcesDirPath, MoonZipFileName);
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            using (var zf = ZipFile.Read(moonZipFilePath))
+                zf.ExtractAll(TestResourcesDirPath);
+            stopWatch.Stop();
+
+            Console.WriteLine("Elapsed time : {0} ms", stopWatch.ElapsedMilliseconds);
+
+            var moonJpg = File.ReadAllBytes(Path.Combine(TestResourcesDirPath, MoonJpgFileName));
+            var moonOriginalJpg = File.ReadAllBytes(Path.Combine(TestResourcesDirPath, MoonOriginalJpgFileName));
+
+            CollectionAssert.AreEqual(moonOriginalJpg, moonJpg);
+
+            File.Delete(Path.Combine(TestResourcesDirPath, MoonJpgFileName));
+        }
+    }
+}
